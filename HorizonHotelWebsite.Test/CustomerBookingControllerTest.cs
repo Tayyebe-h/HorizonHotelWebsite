2f87f6997c0a607f6f5ad487a2d9587a2450e96d
Tayyebe-h <t.hoseini1796@gmail.com>
2020-11-02 15:10:39 +0100

    Some changes to add Test project

---
+++ HorizonHotelWebsite.Test/CustomerBookingControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using HorizonHotelWebsite.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HorizonHotelWebsite.Test
+{
+    public class CustomerBookingControllerTest
+    {
+        public void Index_test()
+        {
+            
+        }
+
+    }
+}
