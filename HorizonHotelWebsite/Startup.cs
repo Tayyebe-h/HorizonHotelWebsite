0e02a8e7bc62e18f3cdbf219d1791b4ab9fc11ca
Tayyebe-h <t.hoseini1796@gmail.com>
2020-11-16 14:29:16 +0100

    Update Customer Booing

--- HorizonHotelWebsite/Startup.cs
+++ HorizonHotelWebsite/Startup.cs
@@ -28,8 +28,7 @@ namespace HorizonHotelWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<DataBaseContext>();
-            
+                       
 
             services.AddScoped<IAdminBookingRepository, AdminBookingRepository>();
             services.AddScoped<IAdminRoomRepo, AdminRoomRepo>();
